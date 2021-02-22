a45a4458f274cb65e11d3cb27b38ef3477730df2
Manikandan <Manikandan@Dell-Inspiron-5482>
2021-02-22 08:24:21 +0530

    TestGitPush

---
+++ E2ESeleniumJenkins/src/testsases/Tc2.java
@@ -0,0 +1,17 @@
+package testsases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	
+	@Test
+	public void testCase() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Selenium\\Drivers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("aravind.win@gmail.com");
+		driver.findElementById("pass").sendKeys("Cyprus@123");
+	}
+
+}
